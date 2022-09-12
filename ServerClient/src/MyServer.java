73f41cbba1bdb28c1051bc8d4dbc45e005924e02
champhusnain <champ.husnain@gmail.com>
2022-09-12 14:43:57 +0500

    Uploading files

---
+++ ServerClient/src/MyServer.java
@@ -0,0 +1,44 @@
+// Server program
+ 
+import java.io.*;
+import java.net.*;
+ 
+class MyServer{
+    public static void main(String[] args)
+    {
+        try {
+           
+            // establish connection
+            ServerSocket serversocket
+                = new ServerSocket(1346);
+ 
+            System.out.println("waiting for request....");
+ 
+            // Socket object to accept all the connections
+            Socket socket = serversocket.accept();
+ 
+            System.out.println("Request Accepted...");
+           
+            // Printstream to print all the data
+            PrintStream ps
+                = new PrintStream(socket.getOutputStream());
+ 
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+           
+            System.out.println(
+                "Input the data at the server...");
+           
+            // Printing bufferedreader data
+            ps.print(br.readLine());
+            socket.close();
+            serversocket.close();
+        }
+        catch (IOException e) {
+           
+            // Catch block for data stream errors
+            System.out.println("Not found data for socket"
+                               + e);
+        }
+    }
+}
