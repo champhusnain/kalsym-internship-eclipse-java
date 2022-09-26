6e9065083129c435635b5eeed62b473e3ad5a870
champhusnain <champ.husnain@gmail.com>
2022-09-26 09:52:26 +0500

    COMIT ALL

--- Testingjavatimer/src/crunchify/com/tutorials/CrunchifyCleanExipredMapElements.java
+++ Testingjavatimer/src/crunchify/com/tutorials/CrunchifyCleanExipredMapElements.java
@@ -21,16 +21,7 @@ public class CrunchifyCleanExipredMapElements {
             crunchifySpyMemCached = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
             log("=====> Approach-1: SpyMemcached <=====\n");
             log("==> Connected to Crunchify's Local Server Successfully." + " Host: localhost, Port: 11211");
-            // Set an object in the cache (using the default transcoder) regardless of any existing value.
-            // The exp value is passed along to memcached exactly as given, and will be processed per the memcached protocol specification:
-            // The actual value sent may either be Unix time (number of seconds since January 1, 1970, as a 32-bit value), or a number of seconds starting from
-            // current time. In the latter case, this number of seconds may not exceed 60*60*24*30 (number of seconds in 30 days); if the number sent by a
-            // client is larger than that, the server will consider it to be real Unix time value rather than an offset from current time.
-            //crunchifySpyMemCached.set("Crunchify", 2000, "New York");
-          //  crunchifySpyMemCached.set("Google", 2000, "Mountain View");
-           // crunchifySpyMemCached.set("PayPal", 2000, "San Jose");
-           // crunchifySpyMemCached.set("Twitter", 2000, "San Francisco");
-           // crunchifySpyMemCached.set("Amazon", 2000, "Seattle");
+
             
             Object myObject = null;
             for (int i = 0; i < 5000000; i++)
@@ -56,16 +47,7 @@ public class CrunchifyCleanExipredMapElements {
                   //  }
             log("==> Total 50M Records added to MemCached");
             
-            // Get with a single key and decode using the default transcoder.
-           // log("Key: Google, Value: " + crunchifySpyMemCached.get("Google"));
-          //  log("Key: PayPal, Value: " + crunchifySpyMemCached.get("PayPal"));
-           // log("Key: Twitter, Value: " + crunchifySpyMemCached.get("Twitter")); 
-           // log("Key: Amazon, Value: " + crunchifySpyMemCached.get("Amazon"));
-           // log("Key: Crunchify, Value: " + crunchifySpyMemCached.get("Crunchify"));
-           // log("==> Total  Records Retrieved from MemCached using net.spy.spymemcached method:\n");
-            // Delete the given key from the cache.
-            //crunchifySpyMemCached.delete("Crunchify");
-            //log("==> Key:Crunchify deleted successfully\n");
+         
             log(crunchifySpyMemCached.getStats(null));
             //log("Key: Crunchify, Value: " + crunchifySpyMemCached.get("Crunchify"));
             //log("==> If no record found, it returns NULL\n");
